4d63147f8eb8fb11b72ea6fc4392df3aff45be34
Phuong Nguyen <phuong.nguyenanh06@gmail.com>
2024-07-15 14:59:02 +0000

    test firebase

--- app.ts
+++ app.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response } from "express";
 import initCrons from "./cronJobs";
+import db from "./firebase";
+import { collection, getDocs } from "firebase/firestore";
 const app = express();
 const port = process.env.PORT || 3001;
 
@@ -18,3 +20,13 @@ server.keepAliveTimeout = 120 * 1000;
 server.headersTimeout = 120 * 1000;
 
 initCrons();
+
+const getUsers = async () => {
+  const querySnapshot = await getDocs(collection(db, "users"));
+  querySnapshot.forEach((doc) => {
+    // doc.data() is never undefined for query doc snapshots
+    console.log(doc.id, " => ", doc.data());
+  });
+};
+
+getUsers();
